dec5849cf849e666aa5134db9bb216547ff68221
Samensai <sam22072003@gmail.com>
2026-04-21 11:08:13 +0200

    Create pigments.js

---
+++ src/data/pigments.js
@@ -0,0 +1,142 @@
+{
+  "pigments": {
+    "PY1":   { "name": "Jaune Arylamide", "lab": [88, 2, 72],   "rgb": [255, 220, 0]   },
+    "PY3":   { "name": "Jaune Arylamide 10G", "lab": [87, -2, 68], "rgb": [252, 215, 10] },
+    "PY35":  { "name": "Jaune de Cadmium", "lab": [85, 10, 90],  "rgb": [255, 215, 0]   },
+    "PY42":  { "name": "Ocre Jaune Synthétique", "lab": [62, 8, 40], "rgb": [198, 150, 60] },
+    "PY74":  { "name": "Jaune Arylamide 5GX", "lab": [90, -5, 82], "rgb": [255, 235, 0] },
+    "PY97":  { "name": "Jaune Arylamide FGL", "lab": [84, 8, 88], "rgb": [255, 200, 0]  },
+    "PY110": { "name": "Jaune Isoindolinone", "lab": [75, 20, 80], "rgb": [220, 170, 10] },
+    "PY150": { "name": "Jaune Nickel Azo", "lab": [72, 15, 68], "rgb": [200, 160, 20]  },
+    "PY153": { "name": "Jaune Complexe Nickel", "lab": [78, 5, 55], "rgb": [210, 180, 80] },
+
+    "PO20":  { "name": "Orange Cadmium", "lab": [65, 38, 60],   "rgb": [230, 120, 20]  },
+    "PO43":  { "name": "Périlène Orange", "lab": [52, 45, 50],  "rgb": [210, 90, 20]   },
+    "PO71":  { "name": "Orange Pyrrole", "lab": [60, 50, 52],   "rgb": [220, 100, 10]  },
+    "PO73":  { "name": "Orange Pyrrole 2", "lab": [58, 48, 55], "rgb": [218, 95, 15]   },
+
+    "PR4":   { "name": "Rouge Chloré Para", "lab": [45, 52, 28], "rgb": [195, 30, 45]  },
+    "PR19":  { "name": "Quinacridone Rouge", "lab": [42, 58, 15], "rgb": [185, 20, 55] },
+    "PR101": { "name": "Oxyde de Fer Rouge", "lab": [36, 22, 20], "rgb": [140, 60, 40] },
+    "PR102": { "name": "Ocre Rouge", "lab": [42, 26, 28],        "rgb": [165, 80, 40]  },
+    "PR108": { "name": "Rouge Cadmium", "lab": [45, 55, 35],     "rgb": [210, 30, 30]  },
+    "PR122": { "name": "Quinacridone Magenta", "lab": [42, 62, -8], "rgb": [190, 15, 80] },
+    "PR170": { "name": "Rouge Naphtol", "lab": [40, 55, 30],     "rgb": [195, 25, 35]  },
+    "PR179": { "name": "Périlène Rouge", "lab": [35, 48, 32],    "rgb": [175, 20, 25]  },
+    "PR209": { "name": "Quinacridone Rouge Y", "lab": [43, 55, 20], "rgb": [200, 25, 50] },
+    "PR254": { "name": "Rouge Pyrrole", "lab": [43, 58, 38],     "rgb": [210, 20, 20]  },
+    "PR255": { "name": "Écarlate Pyrrole", "lab": [48, 58, 42],  "rgb": [220, 30, 15]  },
+
+    "PV19":  { "name": "Quinacridone Violet", "lab": [38, 40, -25], "rgb": [160, 30, 120] },
+    "PV23":  { "name": "Violet Dioxazine", "lab": [28, 32, -35],  "rgb": [80, 20, 130]  },
+    "PV37":  { "name": "Violet Quinacridone", "lab": [35, 38, -22], "rgb": [145, 25, 115] },
+
+    "PB15":  { "name": "Phtalocyanine Bleu GS", "lab": [35, -10, -48], "rgb": [0, 80, 175]  },
+    "PB15:1":{ "name": "Phtalocyanine Bleu RS", "lab": [33, -5, -52],  "rgb": [0, 70, 185]  },
+    "PB15:3":{ "name": "Phtalocyanine Bleu RS+", "lab": [30, 5, -55],  "rgb": [5, 60, 190]  },
+    "PB27":  { "name": "Bleu de Prusse", "lab": [28, -8, -30],  "rgb": [20, 65, 120]   },
+    "PB28":  { "name": "Bleu Cobalt", "lab": [40, -5, -40],     "rgb": [40, 90, 170]   },
+    "PB29":  { "name": "Bleu Outremer", "lab": [32, 10, -48],   "rgb": [30, 40, 175]   },
+    "PB35":  { "name": "Bleu Céruleum", "lab": [55, -12, -30],  "rgb": [80, 145, 195]  },
+    "PB60":  { "name": "Bleu Indanthrène", "lab": [22, 5, -40],  "rgb": [15, 35, 140]  },
+
+    "PG7":   { "name": "Phtalocyanine Vert", "lab": [50, -35, -8], "rgb": [0, 130, 90]  },
+    "PG17":  { "name": "Vert Oxyde de Chrome", "lab": [42, -25, 15], "rgb": [60, 120, 65] },
+    "PG18":  { "name": "Vert Viridien", "lab": [48, -28, 5],    "rgb": [40, 135, 100]  },
+    "PG36":  { "name": "Phtalocyanine Vert Jaune", "lab": [58, -40, 10], "rgb": [10, 160, 80] },
+    "PG50":  { "name": "Vert Titane", "lab": [60, -15, 20],     "rgb": [100, 160, 100] },
+
+    "PBr7":  { "name": "Ombre Naturelle", "lab": [30, 8, 15],   "rgb": [100, 70, 40]   },
+    "PBr25": { "name": "Brun Benzimidazolone", "lab": [38, 20, 30], "rgb": [150, 90, 40] },
+
+    "PBk6":  { "name": "Noir de Carbone", "lab": [10, 0, 0],    "rgb": [20, 20, 20]    },
+    "PBk7":  { "name": "Noir Ivoire", "lab": [12, 2, 4],        "rgb": [25, 22, 18]    },
+    "PBk9":  { "name": "Noir de Vigne", "lab": [15, 2, 5],      "rgb": [30, 25, 20]    },
+    "PBk11": { "name": "Noir Oxyde de Fer", "lab": [16, 2, 3],  "rgb": [28, 24, 22]    },
+
+    "PW4":   { "name": "Blanc de Zinc", "lab": [96, -1, 2],     "rgb": [248, 248, 245] },
+    "PW6":   { "name": "Blanc de Titane", "lab": [97, -1, 1],   "rgb": [252, 252, 252] },
+    "PW7":   { "name": "Blanc de Mica", "lab": [95, -1, 2],     "rgb": [246, 246, 244] }
+  },
+  "brands": {
+    "winsor": {
+      "name": "Winsor & Newton",
+      "series": "Artists' Oil Colour",
+      "colors": [
+        { "name": "Jaune Citron",     "code": "PY3",    "id": "WN-087" },
+        { "name": "Jaune Cadmium Clair", "code": "PY35", "id": "WN-118" },
+        { "name": "Ocre Jaune",       "code": "PY42",   "id": "WN-552" },
+        { "name": "Orange Cadmium",   "code": "PO20",   "id": "WN-089" },
+        { "name": "Rouge Cadmium Clair", "code": "PR108","id": "WN-094" },
+        { "name": "Rouge Alizarine Cramoisie", "code": "PR83","id":"WN-004"},
+        { "name": "Carmin",           "code": "PR19",   "id": "WN-091" },
+        { "name": "Bleu Outremer",    "code": "PB29",   "id": "WN-263" },
+        { "name": "Bleu Cobalt",      "code": "PB28",   "id": "WN-178" },
+        { "name": "Bleu Prussien",    "code": "PB27",   "id": "WN-538" },
+        { "name": "Vert Viridien",    "code": "PG18",   "id": "WN-692" },
+        { "name": "Vert Phtalocyanine","code": "PG7",   "id": "WN-522" },
+        { "name": "Terre d'Ombre Naturelle","code":"PBr7","id":"WN-676"},
+        { "name": "Noir d'Ivoire",    "code": "PBk7",   "id": "WN-331" },
+        { "name": "Blanc de Titane",  "code": "PW6",    "id": "WN-644" }
+      ]
+    },
+    "sennelier": {
+      "name": "Sennelier",
+      "series": "Huile Fine",
+      "colors": [
+        { "name": "Jaune Primaire",   "code": "PY74",   "id": "SEN-573" },
+        { "name": "Jaune d'Or",       "code": "PY150",  "id": "SEN-527" },
+        { "name": "Orange Pur",       "code": "PO71",   "id": "SEN-608" },
+        { "name": "Rouge Vermillon",  "code": "PR255",  "id": "SEN-698" },
+        { "name": "Laque de Garance", "code": "PR19",   "id": "SEN-635" },
+        { "name": "Rose Quinacridone","code": "PR122",  "id": "SEN-690" },
+        { "name": "Violet Dioxazine", "code": "PV23",   "id": "SEN-917" },
+        { "name": "Bleu Outremer Fin","code": "PB29",   "id": "SEN-315" },
+        { "name": "Bleu Céruleum",    "code": "PB35",   "id": "SEN-323" },
+        { "name": "Vert Émeraude",    "code": "PG18",   "id": "SEN-807" },
+        { "name": "Noir de Vigne",    "code": "PBk9",   "id": "SEN-407" },
+        { "name": "Blanc de Zinc",    "code": "PW4",    "id": "SEN-116" }
+      ]
+    },
+    "golden": {
+      "name": "Golden",
+      "series": "Heavy Body Acrylic",
+      "colors": [
+        { "name": "Hansa Yellow Light","code":"PY3",   "id": "G-1116" },
+        { "name": "Hansa Yellow Med.", "code":"PY74",  "id": "G-1119" },
+        { "name": "Yellow Ochre",      "code":"PY42",  "id": "G-1276" },
+        { "name": "Pyrrole Orange",    "code":"PO73",  "id": "G-1310" },
+        { "name": "Pyrrole Red",       "code":"PR254", "id": "G-1321" },
+        { "name": "Quinacridone Magenta","code":"PR122","id":"G-1469" },
+        { "name": "Dioxazine Purple",  "code":"PV23",  "id": "G-1440" },
+        { "name": "Ultramarine Blue",  "code":"PB29",  "id": "G-1379" },
+        { "name": "Phthalo Blue GS",   "code":"PB15",  "id": "G-1381" },
+        { "name": "Cobalt Blue",       "code":"PB28",  "id": "G-1390" },
+        { "name": "Phthalo Green BS",  "code":"PG7",   "id": "G-1415" },
+        { "name": "Chromium Oxide Green","code":"PG17","id":"G-1420"  },
+        { "name": "Raw Umber",         "code":"PBr7",  "id": "G-1507" },
+        { "name": "Carbon Black",      "code":"PBk6",  "id": "G-1599" },
+        { "name": "Titanium White",    "code":"PW6",   "id": "G-1690" }
+      ]
+    },
+    "lefranc": {
+      "name": "Lefranc & Bourgeois",
+      "series": "Louvre Huile",
+      "colors": [
+        { "name": "Jaune Citron",     "code": "PY3",   "id": "LB-044" },
+        { "name": "Jaune Cadmium",    "code": "PY35",  "id": "LB-048" },
+        { "name": "Orange Cadmium",   "code": "PO20",  "id": "LB-064" },
+        { "name": "Rouge Vermillon",  "code": "PR4",   "id": "LB-076" },
+        { "name": "Carmin",           "code": "PR19",  "id": "LB-077" },
+        { "name": "Violet",           "code": "PV23",  "id": "LB-096" },
+        { "name": "Bleu Outremer",    "code": "PB29",  "id": "LB-126" },
+        { "name": "Bleu Cobalt",      "code": "PB28",  "id": "LB-127" },
+        { "name": "Vert Émeraude",    "code": "PG18",  "id": "LB-151" },
+        { "name": "Vert Viridien",    "code": "PG7",   "id": "LB-152" },
+        { "name": "Terre d'Ombre",    "code": "PBr7",  "id": "LB-202" },
+        { "name": "Noir Ivoire",      "code": "PBk7",  "id": "LB-225" },
+        { "name": "Blanc de Titane",  "code": "PW6",   "id": "LB-246" }
+      ]
+    }
+  }
+}
